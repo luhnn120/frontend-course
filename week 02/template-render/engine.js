5a6fcbacea8355ffd856995d999ecd59a8080327
luhnn120 <luhnn120@gmial.com>
2021-07-21 18:40:16 +0800

    feature: 子节点处理

--- week 02/template-render/engine.js
+++ week 02/template-render/engine.js
@@ -1,27 +1,27 @@
 class Engine{
   constructor(){}
-  render(templte, data){
+  render(template, data){
     // StringToNode
     // 处理闭合标签
     const regxClosedTag = /<(\w+)\s*([^>]*)>([^<]*)<\/\1>/gm;
     // 处理自闭合标签
     const regxSelfClosedTag = /<(\w+)\s*([^(/>)]*)\/>/gm;
     // 去除换行
-    template = template.replace(/\n/gm, "");
-    while(regxClosedTag.test(templte) || regxSelfClosedTag.test(templte)){
-      templte = templte.replace(regxClosedTag, (match, p1, p2,p3) => {
+    template = template.replace(/\n/gm, "").trim();
+    while(regxClosedTag.test(template) || regxSelfClosedTag.test(template)){
+      template = template.replace(regxClosedTag, (match, p1, p2,p3) => {
         return this.replaceTemplateToNode(p1,p2,p3)
       })
-      templte = templte.replace(regxSelfClosedTag, (match, p1, p2) => {
+      template = template.replace(regxSelfClosedTag, (match, p1, p2) => {
         return this.replaceTemplateToNode(p1,p2)
       })
     }
     // 处理变量
-    // templte = templte.replace(/\{\{(.*?)\}\}/gm, (match, p1, offset, string) => {
+    // template = template.replace(/\{\{(.*?)\}\}/gm, (match, p1, offset, string) => {
     //   const key = p1.trim();
     //   return data[key]
     // })
-    // console.log(templte)
+    // console.log(template)
     // 构建 vnode tree
     // vnode tree => dom tree
     return document.createElement('div')
@@ -31,7 +31,7 @@ class Engine{
     let children = []
     while(/\$\((.*?)\)/.test(content)){
       // 存在children
-      content = content.replace(/\$\((.*)\)/, (match, p1) => {
+      content = content.replace(/\$\((.*?)\)/, (match, p1) => {
         children.push(this.createdChildren(p1))
         return '';
       })
