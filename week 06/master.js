45318b0c7b64b476b2883ab3373b153cb8fd637d
langman <1625726890@qq.com>
2021-08-18 23:16:15 +0800

    feature: 子进程重启

--- week 06/master.js
+++ week 06/master.js
@@ -1,19 +1,60 @@
-const cp = require("child_process")
+const forks = require("child_process").fork
+const cpus = require("os").cpus()
+// master server
+const server = require("net").createServer()
+server.listen(8081)
 
-const child1 = cp.fork("./worker.js")
-const child2 = cp.fork("./worker.js")
-const child3 = cp.fork("./worker.js")
-const child4 = cp.fork("./worker.js")
-const child5 = cp.fork("./worker.js")
-const child6 = cp.fork("./worker.js")
+const workers = {};
 
-var server = require('net').createServer(); 
-server.listen(8081, function () { 
- child1.send('server', server); 
- child2.send('server', server);
- child3.send('server', server); 
- child4.send('server', server);  
- child5.send('server', server); 
- child6.send('server', server); 
- server.close(); 
-}); 
+// 重启次数
+const LIMIT_COUNT = 10
+// 时间限制
+const DURING = 60000
+const restart = []
+let limitLength;
+function isTooFrequently() {
+  // 记录重启时间
+  const time = Date.now()
+  const limitLength = restart.push(time)
+  if (limitLength > LIMIT_COUNT) {
+    restart = restart.slice(LIMIT_COUNT * -1)
+  }
+  // 限制次数内的重启用时与重启次数
+  return restart.length >= LIMIT_COUNT && restart[restart.length - 1] - restart[0] < DURING
+}
+
+function createWorker() {
+  // 重启检测
+  if (isTooFrequently()) {
+    process.emit('giveup', limitLength, DURING)
+    return;
+  }
+  const worker = forks('./worker.js');
+  // 接受异常信号，重启进程
+  worker.on('message', function (message) {
+    if (message.act === 'suicide') {
+      createWorker();
+    }
+  })
+  // 进程退出时重启进程
+  worker.on('exit', function () {
+    console.log(`Worker: ${worker.pid} exited`)
+    delete workers[worker.pid]
+    createWorker();
+  })
+
+  // 句柄转发
+  worker.send('server', server)
+  worker[worker.pid] = worker
+  console.log(`Create worker. pid: ${worker.pid}`)
+}
+
+for (let i = 0; i < cpus.length; i++) {
+  createWorker()
+}
+
+process.on('exit', function () {
+  for (let pid in workers) {
+    workers[pid].kill();
+  }
+})
--- week 06/worker.js
+++ week 06/worker.js
@@ -1,14 +1,26 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) { 
- res.writeHead(200, {'Content-Type': 'text/plain'}); 
- res.end('handled by child, pid is ' + process.pid + '\n'); 
+  setTimeout(() => {
+    res.writeHead(200, {'Content-Type': 'text/plain'}); 
+    res.end('handled by child, pid is ' + process.pid + '\n');
+  }, 5000)
 });
 
+var worker;
 process.on('message', function (m, tcp) { 
-  if (m === 'server') { 
-    tcp.on('connection', function (socket) { 
-    server.emit('connection', socket); 
+  if (m === 'server') {
+    worker = tcp;
+    worker.on('connection', function (socket) { 
+      server.emit('connection', socket); 
   }); 
   } 
-}); 
+});
+
+process.on('uncaughtException', function () {
+  // 发送自杀信号，断开链接，退出进程
+  process.send({act: 'suicide'})
+  worker.close(function () {
+    process.exit(1)
+  })
+})
